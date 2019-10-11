f950bf105a837822f25acf555dd6e44d88c9987f
Mateo Mejía <meteoro.1309@gmail.com>
2019-10-10 22:02:02 -0500

    Storytelling first prototype

--- SELI-Platform/client/main.js
+++ SELI-Platform/client/main.js
@@ -14,13 +14,11 @@ import Home from '../imports/ui/Home';
 import Tutor from '../imports/ui/Tutor';
 import Student from '../imports/ui/Student';
 import Administrator from '../imports/ui/Administrator';
-import AuthoringTool from '../imports/ui/AuthoringTool';
 import TutorRegistration from '../imports/ui/TutorRegistration';
 import Course from '../imports/ui/Course';
 import CoursePreview from '../imports/ui/CoursePreview';
 import UnityWebgl from '../imports/ui/UnityWebgl';
 import MediaPlayer from '../imports/components/student/MediaPlayer';
-import StorytellingTool from '../imports/components/student/storytelling/StorytellingTool';
 import CoursesDashboard from '../imports/ui/CoursesDashboard';
 import TutorRequestList from '../imports/components/administrator/TutorRequestList';
 import CertificateValidation from '../imports/ui/CertificateValidation';
@@ -40,7 +38,6 @@ Meteor.startup(() => {
     <Router history={history}>
       <Switch>
         <Route exact path="/" component={Home} history={history}/>
-        <Route exact path="/authoringTool" component={AuthoringTool} history={history}/>
         <Route exact path="/tutor" component={Tutor} history={history}/>
         <Route exact path="/student" component={Student} history={history}/>
         <Route exact path="/administrator" component={Administrator} history={history}/>
@@ -49,7 +46,6 @@ Meteor.startup(() => {
         <Route exact path="/course" component={Course} history={history}/>
         <Route exact path="/coursePreview" component={CoursePreview} history={history}/>
         <Route exact path="/media" component={MediaPlayer} history={history}/>
-        <Route exact path="/storytelling" component={StorytellingTool} history={history}/>
         <Route exact path="/dashboard" component={CoursesDashboard} history={history}/>
         <Route exact path="/tutorRequests" component={TutorRequestList} history={history}/>
         <Route exact path="/certificatesValidation" component={CertificateValidation} history={history}/>
