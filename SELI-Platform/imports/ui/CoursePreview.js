999e802cf1af2eb883651216e5d556701a8ec511
CAndresH <andres.heredia@uazuay.edu.ec>
2020-04-21 08:50:48 -0500

    mergev2

--- SELI-Platform/imports/ui/CoursePreview.js
+++ SELI-Platform/imports/ui/CoursePreview.js
@@ -18,7 +18,6 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { Courses } from '../../lib/CourseCollection';
-
 import english from '../../lib/translation/english';
 import spanish from '../../lib/translation/spanish';
 import portuguese from '../../lib/translation/portuguese';
