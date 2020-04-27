fc99fc12ac2efc45848e671a5e3066ccc0abd619
juantenesaca <juan170_95@hotmail.es>
2020-04-27 15:10:31 -0500

    Accessibility Configuration Modal #3

--- SELI-Platform/imports/components/course/items/MenuItem.js
+++ SELI-Platform/imports/components/course/items/MenuItem.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Divider from '@material-ui/core/Divider';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -18,30 +16,14 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import BuildIcon from '@material-ui/icons/Build';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
 import SettingsIcon from '@material-ui/icons/Settings';
 import CameraIcon from '@material-ui/icons/Camera';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
-const useStyles = makeStyles(theme => ({
-  typography: {
-    padding: theme.spacing(2),
-  },
-  root: {
-    width: '100%',
-    maxWidth: 300,
-    backgroundColor: theme.palette.background.paper,
-  },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-}));
-
 export default function MenuItem(props) {
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [openList, setOpenList] = React.useState(true);
+  const [openList, setOpenList] = React.useState(false);
 
   function handleDecorative() {
     props.handleDecorative(props.item.id);
@@ -111,7 +93,7 @@ export default function MenuItem(props) {
             <ListSubheader component="div" id="nested-list-subheader" className="list-subheader">
               {props.language[props.item.type].toUpperCase()}
             </ListSubheader>
-          }
+          }list-subheader
           className="menu-item-list"
         >
           <Divider light={false}/>
@@ -129,7 +111,8 @@ export default function MenuItem(props) {
           </ListItem>
           {
             props.item.type === "audio" || props.item.type === "image" || props.item.type === "video" ||props.item.type === "quiz" ?
-              <React.Fragment>
+              <div className="menu-content-accessibility">
+                <Divider light={true}/>
                 <ListItem button onClick={handleClickList}>
                   <ListItemIcon>
                     <AccessibilityNewIcon />
@@ -139,27 +122,26 @@ export default function MenuItem(props) {
                 </ListItem>
                 <Collapse in={openList} timeout="auto" unmountOnExit>
                   <List component="div" disablePadding>
-                    <ListItem disabled={true} button className={classes.nested}>
+                    {/* <ListItem disabled={true} button className="menu-content-accessibility-item">
                       <ListItemIcon>
                         <BuildIcon />
                       </ListItemIcon>
                       <ListItemText primary={props.language.autoRepair} />
-                    </ListItem>
-                    <ListItem onClick={() => handleDecorative()} button className={classes.nested}>
+                    </ListItem> */}
+                    <ListItem onClick={() => handleDecorative()} button className="menu-content-accessibility-item">
                       <ListItemIcon>
                         <CameraIcon />
                       </ListItemIcon>
                       <ListItemText primary={props.language.decorative} />
+                      <ListItemSecondaryAction>
+                        <Checkbox
+                          edge="end"
+                          onClick={() => handleDecorative()}
+                          checked={props.item.attributes.accessibility.pureDecorative}
+                        />
+                      </ListItemSecondaryAction>
                     </ListItem>
-                    <ListItemSecondaryAction>
-                      <Checkbox
-                        edge="end"
-                        onClick={() => handleDecorative()}
-                        checked={props.item.attributes.accessibility.pureDecorative}
-                      />
-                    </ListItemSecondaryAction>
-                    <Divider light={true}/>
-                    <ListItem button onClick={() => this.editAccessibility()} className={classes.nested}>
+                    <ListItem button onClick={() => this.editAccessibility()} className="menu-content-accessibility-item">
                       <ListItemIcon>
                         <SettingsIcon />
                       </ListItemIcon>
@@ -167,7 +149,7 @@ export default function MenuItem(props) {
                     </ListItem>
                   </List>
                 </Collapse>
-              </React.Fragment>
+              </div>
             :
             undefined
           }
