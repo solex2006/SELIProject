c9c0bda5b14c366442732720de37a4a90cebc345
CAndresH <andres.heredia@uazuay.edu.ec>
2020-06-04 17:12:44 -0500

    75 database

--- SELI-Platform/imports/components/course/DesignStep.js
+++ SELI-Platform/imports/components/course/DesignStep.js
@@ -257,7 +257,7 @@ export default function DesignStep(props) {
                       adding: false,
                       editing: false
                     });
-                    return {...prev};
+                    return [...prev];
                   });
                 }}
                 className={classes.saveButton}
