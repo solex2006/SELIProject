583e5e3b7843585cecd40969f74f61d4a6bb1eed
Camilla Silva <ecureuill@users.noreply.github.com>
2020-09-11 21:09:17 -0300

    Update english.js

--- SELI-Platform/lib/translation/english.js
+++ SELI-Platform/lib/translation/english.js
@@ -888,6 +888,6 @@ var ENGLISH = {
   gettingCourseInf: "Getting course information",
   zoomIn: "Zoom in",
   zoomOut: "Zoom out",
-  newEntry: "testing github action file tracker"
+  newEntry: "testing github action file tracker  "
 }
 export default ENGLISH;
