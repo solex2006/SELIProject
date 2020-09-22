c5baf7dbf82915b41787f394f79b398aa344e53c
Hakan Yorulmaz <e.hakan.yorulmaz@hotmail.com>
2020-09-22 23:51:15 +0300

    image and video filter

--- SELI-Platform/lib/ImageProcess.js
+++ SELI-Platform/lib/ImageProcess.js
@@ -1,24 +1,55 @@
+import {
+  Console
+} from 'console';
+
 var pathToFfmpeg = require('ffmpeg-static');
-  const {
-    exec
-  } = require("child_process");
-  const fs = require('fs');
+const {
+  exec
+} = require("child_process");
+const fs = require('fs');
 export function ImageProcess(file, imageWidthSizeByPx) {
-  
-  processCommand = pathToFfmpeg + " -i " + file.path + " -vf scale=" + imageWidthSizeByPx + ":-1 " + file.path +" -y" ;
-  console.log(processCommand);
-  const proc = exec(processCommand, function (error, stdout, stderr) {
-    if (error) throw error;
-    {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
-    }
-  });
-  proc.on('exit', (code) => {
-    proc.kill("SIGABRT");
-    proc.kill("SIGKILL");
-    console.log('Process completed. Result code:  ' + code);
-  });
-  debugger;
-  console.log(pathToFfmpeg);
-};
+  console.log(file)
+  if (file.type.includes("image")) {
+    processCommand = pathToFfmpeg + " -i " + file.path + " -vf scale=" + imageWidthSizeByPx + ":-1 " + file.path + " -y";
+    console.log(processCommand);
+
+    const proc = exec(processCommand, function (error, stdout, stderr) {
+      if (error) throw error; {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+      }
+    });
+
+
+
+    proc.on('exit', (code) => {
+      proc.kill("SIGABRT");
+      proc.kill("SIGKILL");
+      console.log('Process completed. Result code:  ' + code);
+    });
+    debugger;
+    console.log(pathToFfmpeg);
+  }
+  if (file.type.includes("video")) {
+
+    processCommand = pathToFfmpeg + " -i " + file.path + " -vcodec libx264 -crf 27 -preset veryfast  -vf scale=" + imageWidthSizeByPx + ":-1 /home/mfx/SeliCourseFiles/reduced.mp4 -b:v 6M -r 20";
+    console.log(processCommand);
+
+    const proc = exec(processCommand, function (error, stdout, stderr) {
+      if (error) throw error; {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+      }
+    });
+
+
+
+    proc.on('exit', (code) => {
+      proc.kill("SIGABRT");
+      proc.kill("SIGKILL");
+      console.log('Process completed. Result code:  ' + code);
+    });
+    debugger;
+    console.log(pathToFfmpeg);
+  }
+}
