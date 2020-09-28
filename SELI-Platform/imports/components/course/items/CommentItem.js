928a7ce6a5c8e6e62f5248afafce31b3e1f1ca2a
alvaro <alvaro.yapu.cossio@gmail.com>
2020-09-28 11:03:17 -0400

    Add badge form dialog with bugs

--- SELI-Platform/imports/components/course/items/CommentItem.js
+++ SELI-Platform/imports/components/course/items/CommentItem.js
@@ -8,12 +8,21 @@ import Tooltip from '@material-ui/core/Tooltip';
 import { Editor, EditorState, convertFromRaw } from "draft-js";
 import MoreHorizRoundedIcon from '@material-ui/icons/MoreHorizRounded';
 import IconButton from '@material-ui/core/IconButton';
+import QuizForm from '../../content/QuizForm';
+import { FormControl, FormGroup, TextField ,FormControlLabel, Switch, Dialog, DialogTitle} from '@material-ui/core';
+import { Divider } from 'material-ui';
 
 export default class CommentItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      showBadgeForm:false,
+      badgeClass: {
+        name: '',
+        description: '',
+        image: undefined,
+        criteria: '',
+      }
     }
   }
 
@@ -67,8 +76,11 @@ export default class CommentItem extends React.Component {
   }
 
   openBadgeEditor(){
-    // open the editor TODO
+    this.setState({showBadgeForm:true});
     console.log("displayin badge editor")
+  }
+  handleChange(name){
+
   }
   render() {
     return(
@@ -93,8 +105,8 @@ export default class CommentItem extends React.Component {
                       `0${this.props.comment.date.getMinutes()}` : 
                       this.props.comment.date.getMinutes()} - ${this.props.comment.date.toLocaleDateString('en-US')}`}
                   </p>
-                  <IconButton style={{"padding":"0px"}}>
-                    <MoreHorizRoundedIcon onClick={this.openBadgeEditor}/>
+                  <IconButton onClick={this.openBadgeEditor.bind(this)} style={{"padding":"0px"}}>
+                    <MoreHorizRoundedIcon />
                   </IconButton>
                 </div>
                 <p className="student-profile-information-text-secondary-comment">
@@ -129,6 +141,129 @@ export default class CommentItem extends React.Component {
           :
             undefined
         }  
+        {
+          this.state.showBadgeForm ?
+          <Dialog
+            open={open}
+            onClose={this.contentHandleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+            className="dialog"
+            disableBackdropClick={true}
+            disableEscapeKeyDown={true}
+            keepMounted
+            maxWidth={false}
+          >
+            <DialogTitle className="success-dialog-title" id="alert-dialog-title">{"Badge form creation"}</DialogTitle>
+    
+            <div className="dialog-form-container">
+            <div className="quiz-header-container">
+              <div className="quiz-input-container">
+              <TextField
+                id="quiz-input"
+                label={"Badge Form creation"}
+                margin="normal"
+                variant="outlined"
+                className="quiz-input-title"
+                autoFocus={true}
+              />
+            </div>  
+            </div>   
+            <br></br>
+          {/* Badge section */}
+          <Divider />
+          <div className="center-row">
+            <FormControl className="quiz-form-control" component="fieldset">
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch onChange={this.handleChange('allowBadge')} value="allowBadge" />}
+                  label={this.state.showBadgeOptions?this.props.language.removeBadge:this.props.language.addBadge}
+                />
+              </FormGroup>
+            </FormControl>
+          </div>
+           { 
+              this.state.showBadgeOptions ?
+              <div className="badgeCreationOptions">
+                <div className="badge-form-container">
+                  <div className="badge-image-upload ">
+                    {
+                      this.state.showPreview ?
+                        <div className="form-preview-container">
+                          <ImagePreview
+                            file={this.state.attributes.badgeClass.image}
+                            language={this.props.language}
+                            unPickFile={this.unPickBadgeImage.bind(this)}
+                          />
+                        </div>
+                        :
+                        <div className="form-file-container">
+                          <BadgeUpload
+                            type={this.state.fileType}
+                            user={Meteor.userId()}
+                            accept={this.state.accept}
+                            getFileInformation={this.getbadgeClass.bind(this)}
+                            label={this.props.language.selectBadgeImage}
+                          />
+                        </div>
+                    }
+                  </div>
+                  {/*  */}
+  
+                  <div className="badge-form-input-column">
+                    <div className="sign-form">
+                      <TextField
+                        id="name-input"
+                        label={this.props.language.name}
+                        margin="normal"
+                        variant="outlined"
+                        fullWidth
+                        autoComplete={"off"}
+                        required
+                        value={this.state.attributes.badgeClass.name}
+                        onChange={this.handleChange('badgeName')}
+                        error={this.state.showError && this.state.attributes.badgeClass.name === ''}
+                      />
+                      <TextField
+                        id="description-input"
+                        label={this.props.language.description}
+                        margin="normal"
+                        variant="outlined"
+                        fullWidth
+                        autoComplete={"off"}
+                        required
+                        multiline
+                        rows={2}
+                        value={this.state.attributes.badgeClass.description}
+                        onChange={this.handleChange('badgeDescription')}
+                        error={this.state.showError && this.state.attributes.badgeClass.description === ''}
+                      />
+                       <TextField
+                        id="description-input"
+                        label={this.props.language.criteria}
+                        margin="normal"
+                        variant="outlined"
+                        fullWidth
+                        autoComplete={"off"}
+                        required
+                        multiline
+                        rows={3}
+                        value={this.state.attributes.badgeClass.criteria}
+                        onChange={this.handleChange('badgeCriteria')}
+                        error={this.state.showError && this.state.attributes.badgeClass.criteria === ''}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              :
+              undefined
+          }
+        </div>
+          </Dialog>
+          :
+          undefined
+        }
       </div>
     );
   }
