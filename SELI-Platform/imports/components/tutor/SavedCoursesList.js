5683c13e3d184e14299822451bdac1d8dd4328e0
CAndresH <andres.heredia@uazuay.edu.ec>
2020-09-29 20:34:30 -0500

    save courses v3.1

--- SELI-Platform/imports/components/tutor/SavedCoursesList.js
+++ SELI-Platform/imports/components/tutor/SavedCoursesList.js
@@ -29,7 +29,8 @@ export default class CoursesList extends React.Component {
     this.state = {
       myCourses: [],
       loading: true,
-      showBoton:false
+      showBoton:false,
+      upload:''
     }
   }
 
@@ -37,6 +38,7 @@ export default class CoursesList extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     console.log("file upload",this.fileInput.current.files[0])
+    this.setState({showBoton:!this.state.showBoton, upload:'Upload a new course...'})
     var file = this.fileInput.current.files[0]; //assuming 1 file only
     if (!file) return;
 
@@ -234,20 +236,28 @@ export default class CoursesList extends React.Component {
 
                    
                     <form onSubmit={this.handleSubmit} style={{display:''}} >
-                           <label>
-                          Upload new course*
-                          </label>    
+                              
                           <input 
+                          style={{ display: 'none' }}
+                            name="file-3[]" 
+                            id="file-3" 
+                            class="inputfile inputfile-3" 
+                            data-multiple-caption="{count} files selected"
                             accept="zip,application/octet-stream,application/zip,application/x-zip,application/x-zip-compressed"
-                            class="input-file" id="my-file"
                             type="file" 
                             ref={this.fileInput}
-                            onChange={()=>this.setState({showBoton:!this.state.showBoton}) }/>
-                          <label tabindex="0" for="my-file" class="input-file-trigger">Select a file...</label>
+                            onChange={(event)=>{
+                              console.log("evento,", event.target.files[0].name)
+                              this.setState({showBoton:!this.state.showBoton, upload:event.target.files[0].name})
+                            }}/>
+                          {/* <label tabindex="0" for="my-file" class="input-file-trigger">Select a file...</label> */}
                    
+					                <label for="file-3"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> 
+                           <span>{this.state.showBoton===true? this.state.upload: 'Upload a new course'}&hellip;</span></label>
+				
 
                        
-                         <button type="submit"  style={{ display: this.state.showBoton===true? 'flex': 'none' }}>Submit</button>
+                         <button class="boton1" type="submit" style={{ display: this.state.showBoton===true? 'flex': 'none' }}>Upload Course</button>
                        
                       </form>
                    
