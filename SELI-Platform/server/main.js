e0e5c5bc5713c7e857e73fae120070f0167968bd
juantenesaca <juan170_95@hotmail.es>
2020-10-27 12:44:35 -0500

    Deploy file updated

--- SELI-Platform/server/main.js
+++ SELI-Platform/server/main.js
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import { JsonRoutes } from 'meteor/simple:json-routes';
 import '../imports/api/courseFiles';
 import {Courses} from '../lib/CourseCollection';
 import {Activities} from '../lib/ActivitiesCollection';
@@ -346,14 +345,6 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(() => {
-    JsonRoutes.setResponseHeaders({
-      "Cache-Control": "no-store",
-      "Pragma": "no-cache",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
-    });
-
     let smtp_domain = Meteor.settings.private.SMTP_DOMAIN;
     let smtp_port = Meteor.settings.private.SMTP_PORT;
     let smtp_user = Meteor.settings.private.SMTP_USER;
